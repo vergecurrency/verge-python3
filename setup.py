8e1ac0b8c5283aab284867d9a8139acd80c1696d
sunerok <8443713+justinvforvendetta@users.noreply.github.com>
2024-07-16 13:37:10 -0400

    bump simplejson to newest

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     long_description='This package allows performing commands such as listing the current balance'
     ' and sending coins to the verge client from Python3. The communication with the'
     ' client happens over JSON-RPC.',
-    url='https://github.com/vergecurrency/verge-python',
+    url='https://github.com/vergecurrency/verge-python3',
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Console',
@@ -17,6 +17,6 @@ setup(
         'Topic :: Office/Business :: Financial'
     ],
     packages=find_packages("src"),
-	install_requires=['simplejson>=3.8.0'],
+	install_requires=['simplejson'],
     package_dir={'': 'src'}
 )
